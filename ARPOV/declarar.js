c41f18cc9cf26c180131a3582996d645dffbcc5c
diezmilseres <50533048+diezmilseres@users.noreply.github.com>
2023-11-08 20:21:10 -0300

    declarar

--- ARPOV/declarar.js
+++ ARPOV/declarar.js
@@ -6,23 +6,10 @@ jQuery(function($) {
       function obtentores(element) {
         jQuery(element).selectize({
             options: [
-              { manufacturer: 'nintendo', value: "nintendo: Nintendo Entertainment System", name: "Nintendo Entertainment System" },
-              { manufacturer: 'nintendo', value: "snes", name: "Super Nintendo Entertainment System" },
-              { manufacturer: 'nintendo', value: "n64", name: "Nintendo 64" },
-              { manufacturer: 'nintendo', value: "gamecube", name: "GameCube" },
-              { manufacturer: 'nintendo', value: "wii", name: "Wii" },
-              { manufacturer: 'microsoft', value: 'xss', name: 'Xbox Series S' },
-              { manufacturer: 'nintendo', value: "wiiu", name: "Wii U" },
-              { manufacturer: 'nintendo', value: "switch", name: "Switch" },
-              { manufacturer: 'sony', value: 'ps1', name: 'PlayStation' },
-              { manufacturer: 'sony', value: 'ps2', name: 'PlayStation 2' },
-              { manufacturer: 'sony', value: 'ps3', name: 'PlayStation 3' },
-              { manufacturer: 'sony', value: 'ps4', name: 'PlayStation 4' },
-              { manufacturer: 'sony', value: 'ps5', name: 'PlayStation 5' },
-              { manufacturer: 'microsoft', value: 'xbox', name: 'Xbox' },
-              { manufacturer: 'microsoft', value: '360', name: 'Xbox 360' },
-              { manufacturer: 'microsoft', value: 'xbone', name: 'Xbox One' },
-              { manufacturer: 'joaquin', value: 'xsx', name: 'Xbox Series X' }
+              { obtentores: 'ACEITERA GENERAL DEHEZA S.A.', value: "ACEITERA GENERAL DEHEZA S.A.: VERANO 23	SOJA", name: "VERANO 23	SOJA" },
+              { obtentores: 'ACEITERA GENERAL DEHEZA S.A.', value: "ACEITERA GENERAL DEHEZA S.A.: VERANO 22	SOJA", name: "VERANO 22	SOJA" },
+              { obtentores: 'ACOPIO AREQUITO SOCIEDAD ANONIMA', value: "ACOPIO AREQUITO SOCIEDAD ANONIMA: INVIERNO 23	CEBADA", name: "INVIERNO 23	CEBADA" },
+              { obtentores: 'ACOPIO AREQUITO SOCIEDAD ANONIMA', value: "ACOPIO AREQUITO SOCIEDAD ANONIMA: INVIERNO 22	CEBADA", name: "INVIERNO 22	CEBADA" },
             ],
             optionGroupRegister: function (optgroup) {
               var capitalised = optgroup.charAt(0).toUpperCase() + optgroup.substring(1);
@@ -35,9 +22,9 @@ jQuery(function($) {
               return group;
             },
             placeholder: 'Click here to select ...',
-            optgroupField: 'manufacturer',
+            optgroupField: 'obtentores',
             labelField: 'name',
-            searchField: ['name', 'manufacturer'],
+            searchField: ['name', 'obtentores'],
             sortField: 'name'
           });
       }
